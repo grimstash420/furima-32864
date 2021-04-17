43137ade38790296774fe214d83810e4e39e8a04
grimstash420 <desupurin@icloud.com>
2021-04-17 14:37:59 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable  
+             
+   validates :password, presence: true, length: { minimum: 6 }
+   validates :email, uniqueness: true
+   validates :nickname, presence: true
+   validates :last_name, presence: true, format: { with: /\^[ぁ-んァ-ン一-龥]/ } 
+   validates :first_name, presence: true, format: { with: /\^[ぁ-んァ-ン一-龥]/ } 
+   validates :last_name_kana, presence: true
+   validates :first_name_kana, presence: true
+   validates :birth_day, presence: true
+       
 
-  validates :email, uniqueness: true
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name, presence: true
-  validates :birth_day, presence: true
-  
 end
